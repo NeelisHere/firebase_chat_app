cd02bec8c33d435594dc263b11d84932202d999a
Neelabh Paul <npaul_be19@thapar.edu>
2023-09-03 17:35:02 +0530

    dynamic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,21 +2,23 @@ import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { signOut } from "firebase/auth"
 import { auth } from "../firebase"
+import { useContext } from 'react'
+import { AuthContext } from '../context/authContext'
 
 
 const Navbar = () => {
-
+    const { currentUser } = useContext(AuthContext) 
 
     return (
         <div className="navbar">
             <div className="user">
                 <img 
-                    src="https://images.pexels.com/photos/846741/pexels-photo-846741.jpeg?auto=compress&cs=tinysrgb&w=600" 
+                    src={currentUser.photoURL}
                     alt="x" />
                 {/* <FontAwesomeIcon icon={faUser} /> */}
                 <div className="userInfo">
-                    <span className="userName">John Doe</span>
-                    <span className="userEmail">johndoe@humans.com</span>
+                    <span className="userName">{currentUser.displayName}</span>
+                    <span className="userEmail">{currentUser.email}</span>
                 </div>
             </div>
             <button
