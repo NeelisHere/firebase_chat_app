da65dc333412732f8e52898213e7e4a4a8537d2e
Neelabh Paul <npaul_be19@thapar.edu>
2023-09-03 18:54:16 +0530

    search feature added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,72 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
+import { collection, or, query, where, getDocs } from "firebase/firestore";
+import { db } from '../firebase'
+import { toast } from "react-hot-toast";
 
 const Search = () => {
+    const [searchText, setSearchText] = useState('')
+    const [searchedUser, setSearchedUser] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    const handleSearch = async (e) => {
+        if (e.code === 'Enter') {
+            const q = query(
+                collection(db, "users"),
+                or(
+                    where("username", "==", searchText),
+                    where("email", "==", searchText)
+                )
+            );
+            try {
+                setLoading(true)
+                const querySnapshot = await getDocs(q);
+                // console.log(querySnapshot)
+                if (querySnapshot.docs.length === 0) {
+                    setSearchText('')
+                    setSearchedUser(null)
+                }
+                querySnapshot.forEach((doc) => {
+                    setSearchedUser(doc.data())
+                });
+            } catch (error) {
+                console.log(error)
+                toast.error('Some error occurred!')
+            } finally {
+                setLoading(false)
+            }
+        }
+    }
     return (
         <div className='search'>
             <div className="searchForm">
                 <FontAwesomeIcon icon={faMagnifyingGlass} />
-                <input type="text" placeholder="Find a User" />
-            </div>
-            <div className="userChat">
-                <img 
-                    src="https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=600" 
-                    alt="x" 
+                <input
+                    type="text"
+                    placeholder="Find a User"
+                    onChange={(e) => setSearchText(e.target.value)}
+                    onKeyDown={handleSearch}
                 />
-                <div className="userChatInfo">
-                    <span>Jane Doe</span>
-                    <span className="userChatEmail">janedoe@humans.com</span>
-                </div>
             </div>
+            {
+                searchedUser &&
+                (
+                    loading ? 
+                    <p style={{textAlign: "center", marginBottom: '10px'}}>Loading...</p> :
+                    <div className="userChat">
+                        <img
+                            src={searchedUser.photoURL}
+                            alt="x"
+                        />
+                        <div className="userChatInfo">
+                            <span>{searchedUser.username}</span>
+                            <span className="userChatEmail">{searchedUser.email}</span>
+                        </div>
+                    </div>
+                )
+            }
+
         </div>
     )
 }
